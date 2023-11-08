9939dbd0862ed8d5c42799c7c129fe371a6d680c
alyhenr <alyssonhra99@gmail.com>
2023-11-08 11:51:54 -0300

    feat: on create post -> upload image to uploadthings working + editor for post content

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { PostCreationRequest, PostValidator } from "@/lib/validators/post";
-import React, { useCallback, useRef } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import TextareaAutosize from "react-textarea-autosize";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import type EditorJS from "@editorjs/editorjs";
+import { uploadFiles } from "@/lib/uploadthing";
 
 type EditorProps = { subredditId: string };
 
@@ -25,12 +26,19 @@ const Editor = ({ subredditId }: EditorProps) => {
   });
 
   const ref = useRef<EditorJS>();
+  const [isMounted, setIsMounted] = useState<boolean>();
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsMounted(true);
+    }
+  }, []);
 
   const initializeEditor = useCallback(async () => {
     const EditorJS = (await import("@editorjs/editorjs")).default;
     const Header = (await import("@editorjs/header")).default;
     const Embed = (await import("@editorjs/embed")).default;
-    const List = (await import("@editorjs/table")).default;
+    const List = (await import("@editorjs/list")).default;
+    const Table = (await import("@editorjs/table")).default;
     const Code = (await import("@editorjs/code")).default;
     const LinkTool = (await import("@editorjs/link")).default;
     const InlineCode = (await import("@editorjs/inline-code")).default;
@@ -56,12 +64,41 @@ const Editor = ({ subredditId }: EditorProps) => {
           },
           image: {
             class: ImageTool,
-            config: { uploader: { async uploadByFile(file: File) {} } },
+            config: {
+              uploader: {
+                async uploadByFile(file: File) {
+                  const [res] = await uploadFiles([file], "imageUploader");
+
+                  return {
+                    success: 1,
+                    file: { url: res.fileUrl },
+                  };
+                },
+              },
+            },
           },
+          list: List,
+          code: Code,
+          inlineCode: InlineCode,
+          table: Table,
+          embed: Embed,
         },
       });
     }
   }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      await initializeEditor();
+
+      setTimeout(() => {});
+    };
+
+    if (isMounted) {
+      init();
+      return () => {};
+    }
+  }, [isMounted, initializeEditor]);
 
   return (
     <div className="w-full p-4 bg-zinc rounded-lg border border-zinc-200">
@@ -71,6 +108,7 @@ const Editor = ({ subredditId }: EditorProps) => {
             placeholder="Title"
             className="w-full resize-none appearance-none overflow-hidden bg-transparent text-4xl font-bold focus:outline-none"
           />
+          <div id="editor" className="min-h-[500px]" />
         </div>
       </form>
     </div>
